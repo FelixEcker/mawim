8e772b0b425ed8301c1a22468d50f0309e5718ab
FelixEcker <felix0eckert0.online.de@gmail.com>
2024-03-18 00:10:23 +0100

    fix --release flag for run.bash

--- run.bash
+++ run.bash
@@ -23,11 +23,12 @@ fi
 
 MAWIM_BIN="build/debug/mawim"
 
-if argtest –-release "$@"; then
+if argtest --release "$@"; then
   MAWIM_BIN="build/release/mawim"
   if [[ ! -f $MAWIM_BIN ]]; then
     mb -n $MARIEBUILD_FLAGS -t release || exit 127
   fi
+  echo $MAWIM_BIN
 else 
   if [[ ! -f $MAWIM_BIN ]]; then
     mb -n $MARIEBUILD_FLAGS -t debug || exit 127
@@ -40,4 +41,4 @@ export MAWIM_BIN
 XEPHYR=$(command -v Xephyr)
 
 echo "==> Running..."
-xinit ./data/xinitrc -- "$XEPHYR" :100 -ac -screen 1920x1080 -host-cursor
+xinit ./data/xinitrc -- "$XEPHYR" :100 -ac -screen 3840x2160 -host-cursor
