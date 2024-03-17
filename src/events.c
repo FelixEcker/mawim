b6e8819b0de3c54bd21595eea00ffaca3b61483f
FelixEcker <felix0eckert0.online.de@gmail.com>
2024-03-17 22:05:25 +0100

    basic handling for window creation

--- src/events.c
+++ src/events.c
@@ -8,10 +8,62 @@ void handle_button_press(mawim_t *mawim, XEvent event) {
   mawim_x11_flush(mawim);
 }
 
+void handle_create_notify(mawim_t *mawim, XEvent event) {
+  mawim_log(LOG_DEBUG, "Got CreateNotify!\n");
+}
+
+void handle_destroy_notify(mawim_t *mawim, XEvent event) {
+  mawim_log(LOG_DEBUG, "Got DestroyNotify!\n");
+}
+
+void handle_reparent_notify(mawim_t *mawim, XEvent event) {
+  mawim_log(LOG_DEBUG, "Got ReparentNotify!\n");
+}
+
+void handle_configure_request(mawim_t *mawim, XConfigureRequestEvent event) {
+  mawim_log(LOG_DEBUG, "Got ConfigureRequest!\n");
+
+  XWindowChanges changes;
+  changes.x = event.x;
+  changes.y = event.y;
+  changes.width = event.width;
+  changes.height = event.height;
+  changes.border_width = event.border_width;
+  changes.sibling = event.above;
+  changes.stack_mode = event.detail;
+
+  XConfigureWindow(mawim->display, event.window, event.value_mask, &changes);
+  mawim_logf(LOG_DEBUG, "Configured Window 0x%08x to dimensions %dx%d\n",
+            event.window, event.width, event.height);
+}
+
+void handle_map_request(mawim_t *mawim, XMapRequestEvent event) {
+  mawim_log(LOG_DEBUG, "Got MapRequest!\n");
+
+  XMapWindow(mawim->display, event.window);
+
+  mawim_logf(LOG_DEBUG, "Mapped Window 0x%08x\n", event.window);
+}
+
 bool mawim_handle_event(mawim_t *mawim, XEvent event) {
   switch (event.type) {
   case ButtonPress:
     handle_button_press(mawim, event);
+    return true;
+  case CreateNotify:
+    handle_create_notify(mawim, event);
+    return true;
+  case DestroyNotify:
+    handle_destroy_notify(mawim, event);
+    return true;
+  case ReparentNotify:
+    handle_reparent_notify(mawim, event);
+    return true;
+  case ConfigureRequest:
+    handle_configure_request(mawim, event.xconfigurerequest);
+    return true;
+  case MapRequest:
+    handle_map_request(mawim, event.xmaprequest);
     return true;
   default:
     return false;
