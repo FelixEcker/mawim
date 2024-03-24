6822079ca85b63551438446f06cf3d0e87110d8a
FelixEcker <felix0eckert0.online.de@gmail.com>
2024-03-24 19:07:52 +0100

    finish refactor

--- mawimctl/src/main.c
+++ mawimctl/src/main.c
@@ -2,11 +2,18 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void panic(char *msg) {
   fprintf(stderr, "mawimctl panic'd: %s\n", msg);
   exit(EXIT_FAILURE);
 }
+
+struct handler {
+  char  *cmd_name;
+  char  *params_str; /* used or help output */
+  int  (*handler)(mawimctl_connection_t*, int argc, char **argv);
+};
 
 /* clang-format off */
 
@@ -42,18 +49,7 @@ void panic(char *msg) {
 
 /* clang-format on */
 
-void list_commands() {
-  fprintf(stderr, "Usage: mawimctl <command [parameters]>\n");
-  fprintf(stderr, "Commands:\n");
-  fprintf(stderr, "\tget_version\n");
-  fprintf(stderr, "\tget_workspace\n");
-  fprintf(stderr, "\tset_workspace <workspace number>\n");
-  fprintf(stderr, "\treload\n");
-  fprintf(stderr, "\tclose_focused\n");
-  fprintf(stderr, "\tmove_focused_to_workspace <workspace number>\n");
-}
-
-int get_version(mawimctl_connection_t *connection) {
+int get_version(mawimctl_connection_t *connection, int argc, char **argv) {
   mawimctl_command_t cmd = {.command_identifier = MAWIMCTL_GET_VERSION,
                             .flags = 0,
                             .data_length = 0,
@@ -66,19 +62,49 @@ int get_version(mawimctl_connection_t *connection) {
   return 0;
 }
 
+const struct handler cmd_handlers[] = {
+  {.cmd_name = "close_focused", .params_str = "", .handler = NULL},
+  {.cmd_name = "get_version", .params_str = "", .handler = &get_version},
+  {.cmd_name = "get_workspace", .params_str = "", .handler = NULL},
+  {.cmd_name = "move_focused_to_workspace", .params_str = "<workspace number>", .handler = NULL},
+  {.cmd_name = "reload", .params_str = "", .handler = NULL},
+  {.cmd_name = "set_workspace", .params_str = "<workspace number>", .handler = NULL},
+};
+
+const int cmd_handlers_count = sizeof(cmd_handlers) / sizeof(struct handler);
+
+void list_commands() {
+  fprintf(stderr, "Usage: mawimctl <command [parameters]>\n");
+  fprintf(stderr, "Commands:\n");
+  for (int i = 0; i < cmd_handlers_count; i++) {
+    fprintf(stderr, "\t%s %s\n", cmd_handlers[i].cmd_name, cmd_handlers[i].params_str);
+  }
+}
+
 int main(int argc, char **argv) {
   if (argc == 1) {
     list_commands();
     return 0;
   }
 
-  mawimctl_connection_t *connection = mawimctl_client_connect(NULL);
-  if (connection == NULL) {
-    return false;
+  int ret = -1;
+
+  for (int i = 0; i < cmd_handlers_count; i++) {
+    if (strcmp(argv[1], cmd_handlers[i].cmd_name) == 0) {
+      mawimctl_connection_t *connection = mawimctl_client_connect(NULL);
+      if (connection == NULL) {
+        macro_connection_null_panic();
+      }
+
+      ret = cmd_handlers[i].handler(connection, argc, argv);
+      free(connection);
+      break;
+    }
   }
 
-  int ret = get_version(connection);
+  if (ret == -1) {
+    fprintf(stderr, "no such command!\n");
+  }
 
-  free(connection);
   return ret;
 }
