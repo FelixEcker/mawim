e49fbcbfa4d740ab67e4ee60d23a4ecf15ebb929
Marie <marie@ha.si>
2024-04-04 21:27:18 +0200

    implement mawim_find_window_in_workspaces

--- src/workspace.c
+++ src/workspace.c
@@ -7,6 +7,9 @@
 
 #include "workspace.h"
 
+#include "mawimctl.h"
+#include "window.h"
+
 void mawim_activate_workspace(mawim_t *mawim,
                               mawimctl_workspaceid_t workspace) {}
 
@@ -18,5 +21,21 @@ mawim_window_t *
 mawim_find_window_in_workspaces(mawim_t *mawim, Window x11_window,
                                 window_list_t **out_window_list,
                                 mawimctl_workspaceid_t *out_workspaceid) {
+  for (mawimctl_workspaceid_t wid = 0; wid < mawim->workspace_count; wid++) {
+    mawim_window_t *win = mawim_find_window(&mawim->workspaces[wid].windows, x11_window);
+    if (win == NULL) {
+      continue;
+    }
+
+    if (out_window_list != NULL) {
+      *out_window_list = &mawim->workspaces[wid].windows;
+    }
+
+    if (out_workspaceid != NULL) {
+      *out_workspaceid = wid;
+    }
+
+    return win;
+  }
   return NULL;
 }
